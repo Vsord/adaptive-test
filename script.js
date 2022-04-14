327ffdab05831686807aa5b55cf5d80bde9ca112
Vsord <stanisla060977@gmail.com>
2022-04-14 22:02:03 +0300

    adaptive-test

---
+++ script.js
@@ -0,0 +1,31 @@
+// let createdElement = document.createElement('p');
+//
+// function addText() {
+//     createdElement.innerText = 'Hello';
+//     document.body.appendChild(createdElement)
+// }
+// addText()
+
+// let paragraph = document.getElementById('paragraph');
+// let button = document.getElementById('button');
+//
+// function addString() {
+//     button.addEventListener('click', () => {
+//         if (paragraph.innerText === '') {
+//             paragraph.innerText = 'hello'.toUpperCase()
+//             button.style.backgroundColor = 'red'
+//         } else {
+//             paragraph.innerText = ''
+//             button.style.backgroundColor = 'white'
+//         }
+//
+//
+//     })
+// }
+
+// let headerElem = document.getElementById('header__menu_id');
+//
+// (() => {
+//     headerElem.style.display = 'inline-block';
+// })();
+
